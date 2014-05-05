fe50db94d203cb68b2e1b43b16144e273fc8ef37
hfaran <hamzafaran@hotmail.com>
2014-05-04 20:00:43 -0700

    Add Indent Block

---
+++ progressive/indent.py
@@ -0,0 +1,22 @@
+from progressive.block import Block
+
+
+class Indent(Block):
+    """Indent Block
+
+    :type  length: int
+    :param length: Length of indent
+    :type  character: unicode|str
+    :param character: Character that indent is composed of
+    """
+    def __init__(self, length=4, character=u" "):
+        self._length = length
+        self._repr = character*length
+
+    @property
+    def repr(self):
+        return self._repr
+
+    @property
+    def len(self):
+        return self._length
