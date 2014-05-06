6841e820713a49767e94c2a7d5bf189e8cf3f818
hfaran <hamzafaran@hotmail.com>
2014-05-05 21:31:53 -0700

    Add Percentage and Fraction blocks

---
+++ progressive/blocks/percentage.py
@@ -0,0 +1,36 @@
+from progressive.blocks.block import Block
+from progressive.util import floor
+
+
+class Percentage(Block):
+    """Percentage complete of a task
+
+    :type  decimal_value: float
+    :param decimal_value: 0<=x<=1, usually value/max_value
+    """
+
+    def __init__(self, decimal_value):
+        self._repr = u"{}%".format(str(floor(decimal_value * 100.0)).rjust(3))
+
+    @property
+    def repr(self):
+        return self._repr
+
+    @property
+    def len(self):
+        return len(self._repr)
+
+
+class Fraction(Block):
+    """Fraction complete of a task"""
+
+    def __init__(self, numerator, denominator):
+        self._repr = u"{}/{}".format(numerator, denominator)
+
+    @property
+    def repr(self):
+        return self._repr
+
+    @property
+    def len(self):
+        return len(self._repr)
