96b2632fc1c527b70349b30576583b2610c6a165
hfaran <hamzafaran@outlook.com>
2014-12-03 12:15:26 -0800

    Have a single test dir as opposed to tests per subpackage

---
+++ tests/test_blocks.py
@@ -0,0 +1,34 @@
+from __future__ import division
+
+from progressive.blocks import bar, indent, percentage, title
+
+
+def test__indent():
+    block = indent.Indent(length=4, character=" ")
+    assert block.len == 4
+    assert block.view == "    "
+
+def test__percentage():
+    block = percentage.Percentage(1/2)
+    assert block.view == "50%"
+    assert block.len == 3
+
+def test__fraction():
+    block = percentage.Fraction(1, 2)
+    assert block.view == "1/2"
+    assert block.len == 3
+
+def test__title():
+    _ = "I am title"
+    block = title.Title(_)
+    assert block.len == 10
+    assert block.view == _
+
+def test__title__custom_length():
+    _ = "I am title"
+    block = title.Title(_, 9)
+    assert block.len == 9
+    assert block.view == _
+
+def test_bar():
+    pass
--- tests/test_progressive.py
+++ tests/test_progressive.py
@@ -1,14 +1,14 @@
-from progressive.examples import tree, simple
-from progressive.exceptions import LengthOverflowError
-
-
-class TestExamples(object):
-
-    def test_tree_example(self):
-        try:
-            tree()
-        except LengthOverflowError as e:
-            print("{}: {}".format(type(e), e))
-
-    def test_simple_example(self):
-        simple()
+# from progressive.examples import tree, simple
+# from progressive.exceptions import LengthOverflowError
+#
+#
+# class TestExamples(object):
+#
+#     def test_tree_example(self):
+#         try:
+#             tree()
+#         except LengthOverflowError as e:
+#             print("{}: {}".format(type(e), e))
+#
+#     def test_simple_example(self):
+#         simple()
